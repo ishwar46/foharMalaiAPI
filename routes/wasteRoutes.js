94a46382df0ee50f139247a461b13016d81925e6
Roshan Kumar Khadka <khadkaroshan715@gmail.com>
2024-04-23 20:57:04 +0545

    made some changes

--- routes/wasteRoutes.js
+++ routes/wasteRoutes.js
@@ -3,10 +3,20 @@ const WasteController = require("../controllers/wasteController")
 const { authGuardAdmin, authGuard } = require("../middleware/authGuard")
 const Waste = require("../model/WasteModel")
 
+
+//Create Waste Produts
 router.post('/create_waste_product', authGuardAdmin, WasteController.createWasteProduct)
-router.get('/waste_product/:id', authGuard, WasteController.getSingleWasteProduct)
-router.get('/all_waste_product', authGuard, WasteController.getAllWasteProducts)
+
+//Get single Produts
+router.get('/waste_product/:id', WasteController.getSingleWasteProduct)
+
+//Get all Produts
+router.get('/all_waste_product', WasteController.getAllWasteProducts)
+
+//Update Waste Produts
 router.put('/update_waste_product/:waste_id', authGuardAdmin, WasteController.updateWasteProduct)
+
+//Delete Waste Produts
 router.delete('/delete/:id', authGuardAdmin, WasteController.deleteWasteProduct)
 
 module.exports = router
