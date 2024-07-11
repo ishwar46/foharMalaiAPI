5e9f29f8e330e874ffd8c501340563d5b025bd23
Ishwar Chaudhary <ishwarura@gmail.com>
2024-07-11 14:49:10 +0545

    special request update

--- model/specialRequestModel.js
+++ model/specialRequestModel.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const specialRequestSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
-  category: { type: String, required: true },
-  estimatedWaste: { type: String, required: true },
-  preferredTime: { type: String, required: true },
-  preferredDate: { type: String, required: true },
-  additionalInstructions: { type: String },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    category: { type: String, required: true },
+    estimatedWaste: { type: String, required: true },
+    preferredTime: { type: String, required: true },
+    preferredDate: { type: String, required: true },
+    additionalInstructions: { type: String },
 });
 
 const SpecialRequest = mongoose.model("SpecialRequest", specialRequestSchema);
